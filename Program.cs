62fbb0faed2ddc31347f4106f686fbac2b4f68a3
Vinit Suryarao <suryarao.vinit@hotmail.com>
2020-10-25 11:46:45 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Type_Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Implicit Type Casting :-
+            // Automatically convert one type to another, no need to use external function
+            int a = 5;
+            double b = a;
+
+            // Explicit Type Casting :-
+            // Need external function to convert one type to another
+            string x = "123";
+            int y = int.Parse(x);
+        }
+    }
+}
